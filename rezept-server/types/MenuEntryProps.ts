295e873c1596f73f7cc032c49c7fcd0e715e53a4
bennetrr <bennet@bennetr.me>
2023-10-31 23:05:10 +0100

    Removed rezept-server

--- rezept-server/types/MenuEntryProps.ts
+++
@@ -1,9 +0,0 @@
-import {IconDefinition} from "@fortawesome/free-regular-svg-icons";
-
-export default interface MenuEntryProps{
-    text?: string
-    icon?: IconDefinition
-    link: string
-    active?: boolean
-    tooltip?: string
-}
